152c9bc819d34568b0831b5be525ec2abb8bcacd
Derek Thuecks <derek.thuecks@gmail.com>
2025-09-25 13:26:41 +0000

    v0.1.250925.1 - Figure generation fixed, except for single sageplot

--- .devcontainer/installLatex.sh
+++ .devcontainer/installLatex.sh
@@ -6,7 +6,8 @@
 # We use TinyTeX (https://yihui.org/tinytex/)
 wget -qO- "https://yihui.org/tinytex/install-bin-unix.sh" | sh
 
-tlmgr install adjustbox amscdx bold-extra braket bussproofs cancel carlisle cases chessfss circuitikz colortbl enumitem extpfeil fontawesome5 fontaxes gensymb imakeidx jknapltx kastrup lambda-lists listings listingsutf8 marvosym mathalpha mathtools menukeys mhchem microtype musicography newpx newtx nicematrix pdfcol pdfpages pdflscape pgfplots phaistos physics polyglossia pstricks realscripts relsize siunitx skak skaknew smartdiagram snapshot stmaryrd tcolorbox tikz-cd tikzfill titlesec txfonts ulem upquote was xfrac xltxtra xpatch xstring 
+# Thuecks added tabularray and ninecolors on 25 September 2025. Current PreTeXt: 2.29.2
+tlmgr install adjustbox amscdx bold-extra braket bussproofs cancel carlisle cases chessfss circuitikz colortbl enumitem extpfeil fontawesome5 fontaxes gensymb imakeidx jknapltx kastrup lambda-lists listings listingsutf8 marvosym mathalpha mathtools menukeys mhchem microtype musicography newpx newtx nicematrix pdfcol pdfpages pdflscape pgfplots phaistos physics polyglossia pstricks realscripts relsize siunitx skak skaknew smartdiagram snapshot stmaryrd tcolorbox tikz-cd tikzfill titlesec txfonts ulem upquote was xfrac xltxtra xpatch xstring tabularray ninecolors
 
 tlmgr path add
 
